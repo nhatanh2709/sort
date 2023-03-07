b0a1700ad0f8b0b6b600e1b3bc1ead4f06a6171a
nhatanh2709 <127209960+nhatanh2709@users.noreply.github.com>
2023-03-08 00:59:14 +0700

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -8,7 +8,7 @@ int find_index(int x,int y)
 {
     int value=a[y];
     int cnt=x;
-    for(int i=x;i<=y-1;i++)
+    for(int i=x; i<=y-1; i++)
     {
         if(a[i]<value)
         {
@@ -29,11 +29,20 @@ void calc(int x,int y)
 }
 int main()
 {
+    freopen("test.inp","r",stdin);
+    freopen("test.out","w",stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     cin>>n;
-    for(int i=1;i<=n;i++)cin>>a[i];
-    calc(1,n);
-    for(int i=1;i<=n;i++)cout<<a[i]<<" ";
+    int t=10;
+    while(t--)
+    {
+        clock_t start=clock();
+        for(int i=1; i<=n; i++)cin>>a[i];
+        double duration;
+        calc(1,n);
+        duration=(clock()-start)/(double)CLOCKS_PER_SEC;
+        cout<<fixed<<setprecision(4)<<duration<<"\n";
+    }
 }
